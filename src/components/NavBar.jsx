def01f2e733e1b465b79d0a5f53ba81a86aea484
nickolayArbuzov <nickarbuzov@yandex.by>
2019-10-13 19:50:36 +0300

    upload packages

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavBar = () => {
+    return <nav className='nav'>
+    <div>
+      <a href='#'>Profile</a>
+    </div>
+    <div>
+      <a href='#'>Mes</a>
+    </div>
+    <div>
+      <a href='#'>News</a>
+    </div>
+    <div>
+      <a href='#'>Feed</a>
+    </div>
+  </nav>
+}
+
+export default NavBar;
