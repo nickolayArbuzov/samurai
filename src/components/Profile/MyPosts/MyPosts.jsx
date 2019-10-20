101f30fa351969c65cce3f864f8f74dee4722d29
nickolayArbuzov <nickarbuzov@yandex.by>
2019-10-20 16:05:05 +0300

    15lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import pro from './MyPosts.module.css';
+import Post from './Post/Post';
 
 const MyPosts = () => {
     return <div className={pro.posts}>
       <div className={pro.item}>myPosts</div>
-      <div className={pro.item}>newPosts</div>
-      <div className={pro.item}>fiPosts</div>
-      <div className={pro.item}>sePosts</div>
-      <div className={pro.item}>sePosts</div>
-      <div className={pro.item}>sePosts</div>
-      <div className={pro.item}>sePosts</div>
-      <div className={pro.item}>sePosts</div>
-      <div className={pro.item}>sePosts</div>
+      <div className={pro.item}>
+        <textarea name="" id="" cols="30" rows="5"></textarea>
+        <button>add post</button>
+      </div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
       
     </div>
 }
