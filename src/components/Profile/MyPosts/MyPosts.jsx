79840c96cf9397886503680ee40ed932a81e57bc
nickolayArbuzov <nickarbuzov@yandex.by>
2019-10-20 15:38:49 +0300

    15lesson

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import pro from './MyPosts.module.css';
+
+const MyPosts = () => {
+    return <div className={pro.posts}>
+      <div className={pro.item}>myPosts</div>
+      <div className={pro.item}>newPosts</div>
+      <div className={pro.item}>fiPosts</div>
+      <div className={pro.item}>sePosts</div>
+      <div className={pro.item}>sePosts</div>
+      <div className={pro.item}>sePosts</div>
+      <div className={pro.item}>sePosts</div>
+      <div className={pro.item}>sePosts</div>
+      <div className={pro.item}>sePosts</div>
+      
+    </div>
+}
+
+export default MyPosts;
