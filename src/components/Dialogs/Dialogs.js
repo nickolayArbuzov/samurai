9fe1d185822520e61ee6ebec2d2cb9ff066b01de
nickolayArbuzov <nickarbuzov@yandex.by>
2019-11-04 00:26:43 +0300

    21lesson

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' +s.active}>
+                    Kolya
+                </div>
+                <div className={s.dialog}>
+                    Ujin
+                </div>
+                <div className={s.dialog}>
+                    Ira
+                </div>
+                <div className={s.dialog}>
+                    Vadik
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hello
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    Good day
+                </div>
+            </div>
         </div>
     )
 }
