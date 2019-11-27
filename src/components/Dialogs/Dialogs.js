43a469612ba5162b943ccd514579771eebc3d044
nickolayArbuzov <nickarbuzov@yandex.by>
2019-11-27 21:17:47 +0300

    25lesson

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -20,6 +20,14 @@ const Dialogs = (props) => {
         {id:3,name:'Ira'},
         {id:4,name:'Maria'}
     ]
+    let dialogElements = [
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>,
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>,
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>,
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+    ]
+    let dialogsElements = dialogsData
+        .map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
     let messagesData = [
         {id:1,message:'Hi'},
         {id:2,message:'How are you'},
@@ -28,10 +36,7 @@ const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                {dialogElements}
             </div>
             <div className={s.messages}>
                 <Message message={messagesData[0].message}/>
