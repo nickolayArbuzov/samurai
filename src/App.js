260c4d12309efa96c5b7e6cc6d5459217d83eb25
nickolayArbuzov <nickarbuzov@yandex.by>
2019-12-01 14:15:59 +0300

    27lesson

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
-const App = () => {
+const App = (props) => {
   return ( 
     <BrowserRouter>
       <div className='app-wrapper'>
         <Header/>
         <NavBar/>
         <div className='app-wrapper-content'>
-          <Route path='/Profile' component={Profile}/>
-          <Route path='/Dialogs' component={Dialogs}/>
+          {/*<Route path='/Profile' component={Profile}/>
+          <Route path='/Dialogs' component={Dialogs}/>*/}
+          <Route path='/Profile' render={ () => <Profile /> }/>
+          <Route path='/Dialogs' render={ () => <Dialogs /> }/>
           <Route path='/News' component={News}/>
           <Route path='/Music' component={Music}/>
           <Route path='/Settings' component={Settings}/>
