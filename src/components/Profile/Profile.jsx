7792e3529ccd3f487cd1504e310d7c983cb3db7d
nickolayArbuzov <nickarbuzov@yandex.by>
2019-12-01 20:04:43 +0300

    28lesson

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,9 +4,19 @@ import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
+
+  let posts =[
+    {id:1, message:'Hi, how are you?',likeCount:2},
+    {id:2, message:'thanks, i"m fine!',likeCount:1},
+    {id:3, message:'Ok!!',likeCount:3},
+    {id:4, message:'What?',likeCount:1},
+    {id:5, message:'And about you?',likeCount:2},
+    {id:6, message:'What about you?',likeCount:5}
+  ]
+
     return <div>
       <ProfileInfo/>
-      <MyPosts/>
+      <MyPosts posts={posts}/>
     </div>
 }
 
