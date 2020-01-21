4ad597f515d2cd04cc90cdcefd994b6490b02c95
nickolayArbuzov <nickarbuzov@yandex.by>
2020-01-22 00:42:49 +0300

    lesson39

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
-let addPostActionCreator = () => {
-  return {
-    type: 'ADD-POST'
-  }
-}
-
-let updateNewPostTextActionCreator = (text) => {
-  return {
-    type: 'UPDATE-NEW-POST-TEXT', 
-    newText: text
-  }
-}
+import addPostActionCreator from '../../../redux/state';
+import updateNewPostTextActionCreator from '../../../redux/state';
 
 const MyPosts = (props) => {
   
   let postElements = props.posts.map(p => <Post message={p.message} likeCount={p.likeCount}/>)
   let newPostElement = React.createRef();
-  let addPost = ()=>{
+  let addPost = () => {
     props.dispatch(addPostActionCreator());
   }
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
         profilePage:{
@@ -32,7 +35,7 @@ let store = {
         alert('state change');
     },
     dispatch(action){
-        if(action.type === 'ADD-POST'){
+        if(action.type === ADD_POST){
             let newPost = {
                 id: 7,
                 message: this._state.profilePage.newPostText,
@@ -42,7 +45,7 @@ let store = {
             this._state.profilePage.newPostText ='';
             this._callSubscriber(this._state);
         }   
-        else if(action.type === 'UPDATE-NEW-POST-TEXT'){
+        else if(action.type === UPDATE_NEW_POST_TEXT){
             this._state.profilePage.newPostText= action.newText;
             this._callSubscriber(this._state);
         }
@@ -51,6 +54,11 @@ let store = {
         this._callSubscriber = observer;
     },
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+  
+export const updateNewPostTextActionCreator = (text) => 
+    ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
 window.store = store;
 export default store;
