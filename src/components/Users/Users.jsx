082f113adfc99f852899724e2961611e576b6bfb
nickolayArbuzov <nickarbuzov@yandex.by>
2021-01-12 21:59:43 +0300

    lesson66

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -28,24 +28,13 @@ let Users = (props) => {
                     </div>
                     <div>
                         {u.isFollow 
-                            ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={()=>{
-                                props.toggleFollowingProgress(true, u.id);
-                                followAPI.unfollow(u.id).then(data => {
-                                        if (data.resultCode === 0){
-                                            props.follow(u.id);
-                                        }
-                                        props.toggleFollowingProgress(false, u.id);
-                                    });
-                                props.unfollow(u.id)}}>Unfollow</button> 
-                            : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={()=>{
-                                props.toggleFollowingProgress(true, u.id);
-                                followAPI.follow(u.id).then(data => {
-                                        if (data.resultCode === 0){
-                                            props.follow(u.id);
-                                        }
-                                        props.toggleFollowingProgress(false, u.id);
-                                    });
-                                }}>Follow</button>}
+                            ? <button disabled={props.followingInProgress
+                                .some(id => id === u.id)} 
+                                onClick={()=>{props.unfollow(u.id);}}>Unfollow</button>
+
+                            : <button disabled={props.followingInProgress
+                                .some(id => id === u.id)} 
+                                onClick={()=>{props.follow(u.id);}}>Follow</button>}
                     </div>
                 </span>
                 <span>
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,4 +1,5 @@
 import { usersAPI } from '../api/api';
+import { followAPI } from '../api/api';
 
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
@@ -65,8 +66,8 @@ const usersReducer = (state = initialState, action) => {
     }
 }
 
-export const follow = (userId) => ({type: FOLLOW, userId});
-export const unfollow = (userId) => ({type: UNFOLLOW, userId});
+export const followSuccess = (userId) => ({type: FOLLOW, userId});
+export const unfollowSuccess = (userId) => ({type: UNFOLLOW, userId});
 export const setUsers = (users) => ({type: SET_USERS, users});
 export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
 export const setTotalUsersCount = (totalUsersCount) =>({type: SET_TOTAL_USER_COUNT, totalUsersCount});
@@ -80,6 +81,30 @@ export const getUsers = (currentPage, pageSize) => {
             dispatch(toggleIsFetching(false));
             dispatch(setUsers(data.items));
             dispatch(setTotalUsersCount(data.totalCount));
+        });
+    }
+}
+
+export const follow = (userId) => {
+    return (dispatch) => {
+        dispatch(toggleFollowingProgress(true, userId));
+        followAPI.follow(userId).then(data => {
+        if (data.resultCode === 0){
+            dispatch(followSuccess(userId));
+        }
+        dispatch(toggleFollowingProgress(false, userId));
+        });
+    }
+}
+
+export const unfollow = (userId) => {
+    return (dispatch) => {
+        dispatch(toggleFollowingProgress(true, userId));
+        followAPI.unfollow(userId).then(data => {
+        if (data.resultCode === 0){
+            dispatch(unfollowSuccess(userId));
+        }
+        dispatch(toggleFollowingProgress(false, userId));
         });
     }
 }
