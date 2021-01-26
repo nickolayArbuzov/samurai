499bbc7f6db8c2cf9574d22f257192e30bb3db9a
nickolayArbuzov <nickarbuzov@yandex.by>
2021-01-26 23:21:56 +0300

    lesson92

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -8,4 +8,38 @@ describe('ProfileStatus component', () => {
         const instance = Component.getInstance();
         expect(instance.state.status).toBe('it-Be');
     });
+    test('after creation, <span> should be displayed', () => {
+        const Component = create(<ProfileStatus status='it-Be'/>);
+        const root = Component.root;
+        let span = root.findByType('span');
+        expect(span).not.toBeNull();
+    });
+    test('after creation, <input> should not be displayed', () => {
+        const Component = create(<ProfileStatus status='it-Be'/>);
+        const root = Component.root;
+        expect(() => {
+            let input = root.findByType('input');
+        }).toThrow();
+    });
+    test('after creation, <span> should contains correct status', () => {
+        const Component = create(<ProfileStatus status='it-Be'/>);
+        const root = Component.root;
+        let span = root.findByType('span');
+        expect(span.children[0]).toBe('it-Be');
+    });
+    test('input should be dispay in editMode instead of span', () => {
+        const Component = create(<ProfileStatus status='it-Be'/>);
+        const root = Component.root;
+        let span = root.findByType('span');
+        span.props.onDoubleClick();
+        let input = root.findByType('input');
+        expect(input.props.value).toBe('it-Be');
+    });
+    test('callback should be called', () => {
+        const mockCallback = jest.fn();
+        const Component = create(<ProfileStatus status='it-Be' updateStatus={mockCallback}/>);
+        const instance = Component.getInstance();
+        instance.deActivateEditMode();
+        expect(mockCallback.mock.calls.length).toBe(1);
+    });
 });
