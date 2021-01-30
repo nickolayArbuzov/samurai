babe807887fd3fd406bebf522f4e5b1643da65f9
nickolayArbuzov <nickarbuzov@yandex.by>
2021-01-30 22:14:31 +0300

    Updates

---
+++ precache-manifest.9952e640a02fa3be04127fe5c8f45ebf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8483d5549243d14f777ee029e060ea",
+    "url": "/samurai/index.html"
+  },
+  {
+    "revision": "cdbe675beb6f2be74b81",
+    "url": "/samurai/static/css/3.77f12e8a.chunk.css"
+  },
+  {
+    "revision": "64498cd5c3c6fe301472",
+    "url": "/samurai/static/css/4.78a9075e.chunk.css"
+  },
+  {
+    "revision": "bbe61eac0f4d61e40b6f",
+    "url": "/samurai/static/css/5.7d292bda.chunk.css"
+  },
+  {
+    "revision": "4a9f43be4fad6d8569fe",
+    "url": "/samurai/static/css/main.10722442.chunk.css"
+  },
+  {
+    "revision": "33a7aaa7ef92c606280b",
+    "url": "/samurai/static/js/2.a8453798.chunk.js"
+  },
+  {
+    "revision": "cdbe675beb6f2be74b81",
+    "url": "/samurai/static/js/3.8d8eaea8.chunk.js"
+  },
+  {
+    "revision": "64498cd5c3c6fe301472",
+    "url": "/samurai/static/js/4.fac23877.chunk.js"
+  },
+  {
+    "revision": "bbe61eac0f4d61e40b6f",
+    "url": "/samurai/static/js/5.27c5dd7e.chunk.js"
+  },
+  {
+    "revision": "4a9f43be4fad6d8569fe",
+    "url": "/samurai/static/js/main.d137d21b.chunk.js"
+  },
+  {
+    "revision": "41a8e08fddb5f510e43c",
+    "url": "/samurai/static/js/runtime-main.97ce3da0.js"
+  },
+  {
+    "revision": "f5b65636c9ac701ba6a3585ffd333b86",
+    "url": "/samurai/static/media/1200px-Smiley.svg.f5b65636.png"
+  },
+  {
+    "revision": "1a7925ff96284abba19787786c047eb5",
+    "url": "/samurai/static/media/7677edd5a44b10130b8824ca020ba60b.1a7925ff.gif"
+  }
+]);
